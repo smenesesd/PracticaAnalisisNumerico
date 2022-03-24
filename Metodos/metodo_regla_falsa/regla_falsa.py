71ca9aff2dc1d23406c5414bef8a7bfeb4790353
Samuel <samuelmd8@gmail.com>
2022-03-23 21:31:26 -0500

    Implementacion de codigo de regla falsa

--- Metodos/metodo_regla_falsa/regla_falsa.py
+++ Metodos/metodo_regla_falsa/regla_falsa.py
@@ -1,7 +1,7 @@
 import sympy 
 from tabulate import tabulate
 
-def biseccion(xi,fx,xf,tol,optiontol):
+def regla_falsa(xi,fx,xf,tol,optiontol):
     tabla = []
     xi = float(xi)
     xf = float(xf)
@@ -11,7 +11,7 @@ def biseccion(xi,fx,xf,tol,optiontol):
     elif fx.subs(x,xi)*fx.subs(x,xf)>0:
         return "Intervalo no valido"
     else:
-        xm = (xi+xf)/2
+        xm = intercepto(fx,xi,xf)
         error=tol+1.0
         if optiontol:
             error=abs(xm-xi)
@@ -23,7 +23,7 @@ def biseccion(xi,fx,xf,tol,optiontol):
                 xf = xm
             else:
                 xi = xm
-            xm = (xi+xf)/2
+            xm = intercepto(fx,xi,xf)
             if optiontol:
                 error=abs(xm-xi)
             else:
@@ -33,11 +33,14 @@ def biseccion(xi,fx,xf,tol,optiontol):
             return "Xm es raiz",tabla
         else:
             return "Xm es una raiz con tolerancia",tabla
-
+def intercepto(fx,xi,xf):
+    m=(fx.subs(x,xf)-fx.subs(x,xi))/(xf-xi)
+    medio=xi-(fx.subs(x,xi)/m)
+    return medio
 #Main
 x = sympy.Symbol('x')                                               #Definimos una funcion con Sympy
 fdx = x**3-7.51*x**2+18.4239*x-14.8331
-resultado=biseccion(3,fdx,3.5,5*10**-5,False)                                #Evaluamos la funcion
+resultado=regla_falsa(3,fdx,3.5,5*10**-5,True)                                #Evaluamos la funcion
 print(resultado[0])                                                 #Imprimimos el resultado con respecto a la raiz
 print(tabulate(resultado[1],headers=["xi", "f(xi)", "xm","f(xm)","xf","f(xf)","error"]))
 
