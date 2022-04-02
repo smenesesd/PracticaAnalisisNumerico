2983cef5d30193a5d14a05383ca92dbefdf80d6c
dgomezc10 <dgomezc10@eafit.educ.co>
2022-04-02 13:09:20 -0500

    traduccion fixed point

--- Metodos/metodo_punto_fijo/punto_fijo.py
+++ Metodos/metodo_punto_fijo/punto_fijo.py
@@ -1,16 +1,16 @@
 import sympy
 from tabulate import tabulate
 from math import e
-def punto_fijo(fx,xi,tol,optiontol,gx,nitera):
-    tabla = []
+def fixed_point(fx,xi,tol,optiontol,gx,nitera):
+    table = []
     xi = float(xi)
     tol = float(tol)
     if fx.subs(x,xi)==0:
-        return "Xi es raiz"
+        return "Xi is a root"
     else:
         ite = 0
         error = tol+1.0
-        tabla.append([ite,xi,round(gx.subs(x,xi),4),error])
+        table.append([ite,xi,round(gx.subs(x,xi),4),error])
         while(error>=tol and ite < nitera):
             xm = gx.subs(x,xi)
             if optiontol:
@@ -18,16 +18,16 @@ def punto_fijo(fx,xi,tol,optiontol,gx,nitera):
             else:
                 error=abs((xm-xi)/xm)
             ite += 1
-            tabla.append([ite,xm,round(gx.subs(x,xm),4),error])
+            table.append([ite,xm,round(gx.subs(x,xm),4),error])
             xi = xm
         if error<tol:
-            return "Xi es raiz con tolerancia",tabla
+            return "Xi is root with tol",table
         else:
-            return "No llegamos", tabla
+            return "we don't arrived", table
 #main
 x = sympy.Symbol('x') 
 gx = e**(-x)                                              
 fdx = (e**(-x))-x
-resultado=punto_fijo(fdx,0.5,5*10**-8,True,gx,21)                                
-print(resultado[0])                                                 
-print(tabulate(resultado[1],headers=["ite", "x", "g(x)","error"]))
+result=fixed_point(fdx,0.5,5*10**-8,True,gx,21)                                
+print(result[0])                                                 
+print(tabulate(result[1],headers=["ite", "x", "g(x)","error"]))
