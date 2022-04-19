607b298640273613b3d0ba5bd836edbcb42f51b7
dgomezc10 <dgomezc10@eafit.educ.co>
2022-04-18 22:05:57 -0500

    Correcion templates

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Proyecto/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Proyecto/metodoscerrados/urls.py
+++ Proyecto/metodoscerrados/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 from metodoscerrados import views
 urlpatterns = [
-    path(
-        route='incremental/', 
-        view = views.metodo_incremental.as_view(),
-        name='metodo incremental'
-    )
+    path(route='incremental/', view = views.metodo_incremental.as_view(), name='metodo incremental')
 ]
