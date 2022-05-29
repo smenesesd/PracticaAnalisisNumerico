150509878b05ef7c7a46a942bd7156fed4162d67
dgomezc10 <dgomezc10@eafit.educ.co>
2022-05-29 18:09:48 -0500

    ajustes diseño

--- Proyecto/metodoscerrados/urls.py
+++ Proyecto/metodoscerrados/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from metodoscerrados import views
 urlpatterns = [
-    path(route='incremental/', view = views.metodo_incremental.as_view(), name='metodo incremental'),
-    path(route='biseccion/', view = views.metodo_biseccion.as_view(), name='metodo biseccion'),
-    path(route='regla/', view = views.metodo_regla_faslsa.as_view(), name='metodo regla falsa'),
+    path(route='incremental/', view = views.metodo_incremental.as_view(), name='incremental'),
+    path(route='biseccion/', view = views.metodo_biseccion.as_view(), name='biseccion'),
+    path(route='regla/', view = views.metodo_regla_faslsa.as_view(), name='regla_falsa'),
 ]
