7bbe66b6bb1606778a4ba0055eba93f467cfd62c
Samuel <samuelmd8@gmail.com>
2022-05-29 18:38:29 -0500

    Formulario de regla falsa y incremental, y html

--- Proyecto/metodoscerrados/views.py
+++ Proyecto/metodoscerrados/views.py
@@ -2,6 +2,8 @@ from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
 from django.views import View
 from metodoscerrados.forms import Formulario_biseccion
+from metodoscerrados.forms import Formulario_regla_falsa
+from metodoscerrados.forms import Formulario_incremental
 
 # Create your views here.
 class metodo_incremental(View):
@@ -32,7 +34,37 @@ class metodo_biseccion(View):
 
 class metodo_regla_faslsa(View):
     template_name = 'regla_falsa/reglafalsa.html'
+    form_class = Formulario_regla_falsa()
     
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        form = self.form_class
+        return render(request, self.template_name,{'form':form})
+
+    def post(self, request, *args, **kwargs):
+        form = Formulario_regla_falsa(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['funcion'])
+            print(form.cleaned_data['xi'])
+            print(form.cleaned_data['xf'])
+            print(form.cleaned_data['tolerancia'])
+            print(form.cleaned_data['opcion'])
+        return render(request, self.template_name, {'form':Formulario_regla_falsa})
+
+class metodo_incremental(View):
+    template_name = 'incremental/incremental.html'
+    form_class = Formulario_incremental()
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, self.template_name,{'form':form})
+
+    def post(self, request, *args, **kwargs):
+        form = Formulario_incremental(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['funcion'])
+            print(form.cleaned_data['xi'])
+            print(form.cleaned_data['maxite'])
+            print(form.cleaned_data['difx'])
+        return render(request, self.template_name, {'form':Formulario_incremental})
+
     
