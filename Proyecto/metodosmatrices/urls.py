cffad39cc71eba9b18d9bc6baab06464bfa911fa
dgomezc10 <dgomezc10@eafit.educ.co>
2022-05-31 22:42:37 -0500

    sda

---
+++ Proyecto/metodosmatrices/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from metodoscerrados import views
+urlpatterns = [
+    path(route='incremental/', view = views.metodo_incremental.as_view(), name='incremental'),
+    path(route='biseccion/', view = views.metodo_biseccion.as_view(), name='biseccion'),
+    path(route='regla/', view = views.metodo_regla_faslsa.as_view(), name='regla_falsa'),]
