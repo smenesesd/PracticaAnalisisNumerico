9163071ab1ff49720982ddae2a6e1e740fba2929
Samuel <samuelmd8@gmail.com>
2022-06-01 02:00:41 -0500

    urls metodomatrices

--- Proyecto/metodosmatrices/urls.py
+++ Proyecto/metodosmatrices/urls.py
@@ -3,4 +3,8 @@ from metodosmatrices import views
 urlpatterns = [
     path(route='crout/', view = views.metodo_crout.as_view(), name='crout'),
     path(route='doolittle/', view = views.metodo_doolittle.as_view(), name='dolittle'),
+    path(route='elim_gauss_piv_parcial/', view = views.metodo_elim_gauss_piv_parcial.as_view(), name='elim_gauss_piv_parcial'),
+    path(route='elim_gauss_piv_total/', view = views.metodo_elim_gauss_piv_total.as_view(), name='elim_gauss_piv_total'),
+    path(route='elim_gaussiana/', view = views.metodo_elim_gaussiana.as_view(), name='elim_gaussiana'),
+    path(route='factorizacion_LU/', view = views.metodo_factorizacion_LU.as_view(), name='factorizacion_LU'),
     ]
