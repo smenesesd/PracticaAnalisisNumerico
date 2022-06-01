5d2c8ccdbae35633167405f1b9a98bbf0c8d22b7
Samuel <samuelmd8@gmail.com>
2022-05-31 22:50:01 -0500

    Creacion urls metodos cerrados

--- Proyecto/metodosmatrices/urls.py
+++ Proyecto/metodosmatrices/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from metodoscerrados import views
 urlpatterns = [
-    path(route='incremental/', view = views.metodo_incremental.as_view(), name='incremental'),
-    path(route='biseccion/', view = views.metodo_biseccion.as_view(), name='biseccion'),
-    path(route='regla/', view = views.metodo_regla_faslsa.as_view(), name='regla_falsa'),]
+    path(route='crout/', view = views.metodo_incremental.as_view(), name='crout'),
+    path(route='doolittle/', view = views.metodo_biseccion.as_view(), name='dolittle'),
+    path(route='elim_gaussiana/', view = views.metodo_regla_faslsa.as_view(), name='elim_gaussiana'),
+    path(route='elim_gauss_piv_parcial/', view = views.metodo_regla_faslsa.as_view(), name='elim_gauss_piv_parcial'),
+    path(route='elim_gauss_piv_total/', view = views.metodo_regla_faslsa.as_view(), name='elim_gauss_piv_total'),
+    path(route='factorizacion_LU/', view = views.metodo_regla_faslsa.as_view(), name='factorizacion_LU'),
+    path(route='seidel/', view = views.metodo_regla_faslsa.as_view(), name='seidel'),]
